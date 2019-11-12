3de272e24529e5ff9e0e6598837e31c825b8a539
qwrwed <razk@hotmail.co.uk>
2019-11-12 02:32:10 +0000

    Completely refactor code

--- client.py
+++ client.py
@@ -1,12 +1,36 @@
 import sys
 import json
+import datetime
 from socket import *
 
 def getCommandLineArgs(defaultServerAddress, defaultServerPort):
     if len(sys.argv) == 3:
-        return sys.argv[1], int(sys.argv[2])
+        return (sys.argv[1], int(sys.argv[2]))
     else:
-        return defaultServerAddress, defaultServerPort
+        return (defaultServerAddress, defaultServerPort)
+    
+def makeRequest(serverInfo, requestType, args = None):
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.settimeout(serverInfo[1])
+    try:
+        clientSocket.connect(serverInfo[0])
+    except ConnectionRefusedError:
+        print("Error: Server unavailable. Exiting...")
+        sys.exit()
+    request = {'requestType': requestType, 'args' : args}
+    clientSocket.send(json.dumps(request).encode())
+    try:
+        [response, successFlag] = json.loads(clientSocket.recv(1024).decode())
+    except timeout:
+        print("Error: Connection timed out (no response within {}s). Exiting...".format(serverInfo[1]))
+        clientSocket.close()
+        sys.exit()
+    clientSocket.close()
+    if successFlag == True:
+        print("Command \"{}\" successful".format(requestType))
+    else:
+        print("Command \"{}\" unsuccessful".format(requestType))
+    return response, successFlag
 
 def showBoardList(boardList):
     boardListString = 'BOARD LIST:\n'
@@ -15,20 +39,16 @@ def showBoardList(boardList):
         boardListString += "{}. {}".format(str(i), boardList[i])
         if i < len(boardList)-1:
             boardListString += delimiterString
-    boardListString = '\n'
+    boardListString += '\n'
     print(boardListString)
 
 def parseFileTitle(fileTitle):
     #YYYYMMDD-HHMMSS -> YYYY/MM/DD HH/MM/SS
-    dateStringList = list(fileTitle[:15].replace('-', ' '))
-    dateStringList.insert(4, '/')
-    dateStringList.insert(7, '/')
-    dateStringList.insert(13, ':')
-    dateStringList.insert(16, ':')
+    datetimeObject = datetime.datetime.strptime(fileTitle[:15],'%Y%m%d-%H%M%S')
+    dateString = datetimeObject.strftime('%Y/%m/%d %H:%M:%S')
     messageTitle = fileTitle[16:-4].replace('_', ' ')
-    return ''.join(dateStringList), messageTitle
-    
-    
+    return dateString, messageTitle
+
 def showMessageList(boardName, fileList):
     fileCount = len(fileList)
     if fileCount == 0:
@@ -49,78 +69,62 @@ def showMessageList(boardName, fileList):
             messageListString += delimiterString
     print(messageListString)
 
-def disconnect(clientSocket):
-    request = {'requestType': 'QUIT'}
-    clientSocket.send(json.dumps(request).encode())
-    response = clientSocket.recv(1024)
-    print('Message from server:', response.decode())
-    clientSocket.close()
-
+    
 def main():
-    # variables
-    socketTimeout = 5
     defaultServerAddress = '127.0.0.1'
     defaultServerPort = 12000
+    socketTimeout = 10
 
-    serverAddress, serverPort = getCommandLineArgs(defaultServerAddress, defaultServerPort)
-    print('Setting up connection to server at address {}, port {}...'.format(serverAddress, serverPort))
-    
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.settimeout(socketTimeout)
-    
-    try:
-        clientSocket.connect((serverAddress, serverPort))
-    except ConnectionRefusedError:
-        print('ERROR: Connection refused; server is unavailable. Exiting...')
-        return
-    except timeout:
-        print('ERROR: Connection timed out (no response from server within {}s). Exiting...'.format(socketTimeout))
-        return
-    
-    request = {'requestType': 'GET_BOARDS'}
-    clientSocket.send(json.dumps(request).encode())
-    encodedResponse = clientSocket.recv(1024)
-    response = json.loads(encodedResponse.decode())
-    if type(response) == list:
+    serverInfo = [getCommandLineArgs(defaultServerAddress, defaultServerPort), socketTimeout]
+    response, successFlag = makeRequest(serverInfo, 'GET_BOARDS')
+    if successFlag == True:
         boardList = response
         showBoardList(boardList)
     else:
-        print('Error from server: {}.\nDisconnecting...'.format(response))
-        disconnect(clientSocket)
-        print("Exiting...")
+        print(response)
+        print("Board retrieval failed; Exiting program")
         return
-
-    while True:
+    
+    while True:    
         print('OPTIONS (case-sensitive):')
-        print('Type a number {}-{} to show the 100 most recent messages in the specified board.'.format(0, len(boardList)-1))
+        print('Type a board number ({}-{}) to show the 100 most recent messages in the specified board.'.format(0, len(boardList)-1))
         print('Type \"POST\" to create a new message.')
         print('Type \"QUIT\" to quit.')
         userChoice = input('Input: ')
         print()
         if userChoice == 'QUIT':
-            disconnect(clientSocket)
             break
         elif userChoice.isnumeric() and int(userChoice) in range(len(boardList)):
-            args = {}
             boardName = boardList[int(userChoice)]
-            args['boardName'] = boardName
-            request = {'requestType': 'GET_MESSAGES', 'args': args}
-            clientSocket.send(json.dumps(request).encode())
-            response = clientSocket.recv(1024)
-            fileList = json.loads(response.decode())
-            showMessageList(boardName, fileList)
+            args = {'boardName': boardName}
+            response, successFlag = makeRequest(serverInfo, "GET_MESSAGES", args)
+            if successFlag == True:
+                fileList = response
+                showMessageList(boardName, fileList)
+            else:
+                print(response)
             input('Press Enter to continue.')
             print()
         elif userChoice == 'POST':
             args = {}
-            boardNumber = int(input('Enter number of board to post message on: '))
-            args['boardName'] = boardList[boardNumber]
+            try:
+                boardNumber = int(input('Enter number of board to post message on: '))
+                args['boardName'] = boardList[boardNumber]
+            except (IndexError, ValueError):
+                print("Error: Invalid board number.")
+                input('Press Enter to continue.')
+                print()
+                continue
             args['postTitle'] = input('Enter post title: ')
             args['messageContent'] = input('Enter message content: ')
-            request = {'requestType': 'POST_MESSAGE', 'args': args}
-            clientSocket.send(json.dumps(request).encode())
-            response = clientSocket.recv(1024)
-            print('server says:', response.decode())
+            response, successFlag = makeRequest(serverInfo, "POST_MESSAGE", args)
+            print(response)
+            input('Press Enter to continue.')
+            print()
         else:
             input('User input not recognised. Press Enter to continue.')
+            print()
+#        res = makeRequest(serverInfo, 'ECHO', {'string': st})
+#        print(res)
+    
 main()
--- server.py
+++ server.py
@@ -6,88 +6,122 @@ import datetime # date, time
 
 from socket import *
 
-if len(sys.argv) == 3:
-    serverAddress, serverPort = sys.argv[1], int(sys.argv[2])
-else:
-    serverAddress, serverPort = '127.0.0.1', 12000
+#TODO: Spaces in board titles should be replacedwith underscores(“_”).
 
+def getCommandLineArgs(defaultServerAddress, defaultServerPort):
+    if len(sys.argv) == 3:
+        return (sys.argv[1], int(sys.argv[2]))
+    else:
+        return (defaultServerAddress, defaultServerPort)
+
+def sendResponse(response, connectionSocket, successFlag = True):
+    connectionSocket.send(json.dumps((response, successFlag)).encode())
+    print("Response sent to client")
+    
 def getBoardList():
     return next(os.walk('.\\board'))[1]
     # Source: https://stackoverflow.com/a/142535
 
-def getMessages(boardName):
+def getMessages(args):
+    if args == None:
+        return "ERROR: No arguments provided.", False
+    try:
+        boardName = args['boardName']
+    except KeyError as e:
+        return "ERROR: Missing parameter {}".format(e), False
+    if boardName not in getBoardList():
+        print(getBoardList())
+        print(boardName)
+        return "ERROR: Board \"{}\" does not exist.".format(boardName), False
     boardPath = '.\\board\\{}'.format(boardName)
     messageList = next(os.walk(boardPath))[2]
     messageList.sort()
     messageList = messageList[0:100]
-    response = []
+    responseData = []
     for message in messageList:
         if re.search("[0-9]{8}-[0-9]{6}-.+\.txt", message) != None:
             f = open('{}\\{}'.format(boardPath, message), 'r')
-            response.append([message, f.read()])
+            responseData.append([message, f.read()])
             f.close()
-    return response
+    return responseData, True
 
-def postMessage(args):
-    boardName = args['boardName']
-    boardPath = '.\\board\\{}'.format(boardName)
-    messageTitle = args['postTitle']
-    messageContent = args['messageContent']
-    #TODO: Verify board name
-    messageDate = datetime.datetime.now()
-    fileTitle = ''
-    fileTitle += messageDate.strftime('%Y%m%d-%H%M%S')
+def postMessage(args, requestTimestamp):
+    if args == None:
+        return "ERROR: No arguments provided.", False
+    try:
+        boardName = args['boardName']
+        if not boardName in getBoardList():
+            return "ERROR: Board \"{}\" does not exist.".format(boardName), False
+        boardPath = '.\\board\\{}'.format(boardName)
+        messageTitle = args['postTitle']
+        messageContent = args['messageContent']
+    except KeyError as e:
+        return "ERROR: Missing parameter {}".format(e), False
+    fileTitle = requestTimestamp.strftime('%Y%m%d-%H%M%S') #YYYYMMDD-HHMMSS
     fileTitle += '-'
     fileTitle += '{}.txt'.format(messageTitle.replace(' ', '_'))
     
     f = open('{}\\{}'.format(boardPath, fileTitle), 'w+')
     f.write(messageContent)
     f.close()
-    
-    
+    return "Message successfully posted.", True
 
-def startListening():
+def main():
+    defaultServerAddress = '127.0.0.1'
+    defaultServerPort = 12000
+    
+    # ensure message boards are defined:
+    if not os.path.isdir('.\\board'):
+        print("Board folder is missing. Ending process...")
+        return
+    elif len(getBoardList()) == 0:
+        print("No boards defined. Ending process...")
+        return
+
+    # attempt to create server socket
+    serverInfo = getCommandLineArgs(defaultServerAddress, defaultServerPort)
     serverSocket = socket(AF_INET,SOCK_STREAM)
-    serverSocket.bind((serverAddress, serverPort))
+    try:
+        serverSocket.bind(serverInfo)
+    except OSError:
+        print("ERROR: Unavailable/busy port. Ending process...")
+        return
     serverSocket.listen(1)
-    i = 0
+    print("Initialisation finished - starting listening...")
+    
     while True:
-        print('Waiting for client connection...')
+        print("Waiting for a client to connect")
         connectionSocket, addr = serverSocket.accept()
-        while True:
-            print('Waiting on client message...')
-            print(i)
-            i += 1
-            rawRequest = connectionSocket.recv(1024)
-            print("Raw request:", rawRequest)
-            request = json.loads(rawRequest.decode())
-            requestType = request['requestType']
-            print("Request recieved from client: \"{}\"".format(requestType))
-            if 'args' in request:
-                args = request['args']
-                print("Arguments: {}".format(args))
-            if requestType == 'GET_BOARDS':
-                print('Sending board list to client.')
-                response = getBoardList()
-                connectionSocket.send(json.dumps(response).encode())
-            elif requestType == 'GET_MESSAGES':
-                boardName = args['boardName']
-                print('Sending latest messages to client for board "{}"'.format(boardName))
-                response = getMessages(boardName)
-                connectionSocket.send(json.dumps(response).encode())
-            elif requestType == 'QUIT':
-                print('Closing connection to client.')
-                connectionSocket.send('Closing connection to client.'.encode())
-                connectionSocket.close()
-                break
-            elif requestType == 'POST_MESSAGE':
-                postMessage(args)
-                response = 'Message posting finished'
-                connectionSocket.send(json.dumps(response).encode())
-            else:
-                errorString = 'Client request {} not recognised'.format(requestType)
-                print(errorString)
-                response = errorString
-                connectionSocket.send(json.dumps(response).encode())
+        print("Client connected", addr)
+        request = json.loads(connectionSocket.recv(1024).decode())
+        requestTimestamp = datetime.datetime.now()
+        print("Message received from client: {}".format(request))
+        requestType = request['requestType']
+        args = request['args']
+        if requestType == "GET_BOARDS":
+            response, successFlag = getBoardList(), True
+            sendResponse(response, connectionSocket, successFlag)
+        elif requestType == "GET_MESSAGES":
+            response, successFlag = getMessages(args)
+            sendResponse(response, connectionSocket, successFlag)
+        elif requestType == "POST_MESSAGE":
+            response, successFlag = postMessage(args, requestTimestamp)
+            sendResponse(response, connectionSocket, successFlag)
+        else:
+            successFlag = False
+            sendResponse("ERROR: Request ({}) not recognised.".format(requestType), connectionSocket, False)
+        logLine = ""
+        logLine += (addr[0] + ":" + str(addr[1]))
+        logLine += "\t\t"
+        logLine += requestTimestamp.strftime('%Y/%m/%d %H:%M:%S')
+        logLine += "\t\t"
+        logLine += format(requestType, '<12')
+        logLine += "\t\t"
+        logLine += {True: "OK", False: "Error"}[successFlag]
+        logLine += "\n"
 
-startListening()
+        f = open("server.log", 'a+')
+        f.write(logLine)
+        f.close()
+        
+main()
